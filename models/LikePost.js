7621f42fd2a5fc647748bade7fb5d913dcaba528
sscodebug <sscodebug2024@gmail.com>
2025-01-15 16:08:59 +0100

    model refrences the likes of a post

---
+++ models/LikePost.js
@@ -0,0 +1,44 @@
+// Added By Youssef
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+const User = require('./User');
+const Post = require('./Post');
+
+class LikePost extends Model {}
+
+LikePost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    likeDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    users_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+    posts_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Post,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'LikePost',
+    tableName: 'likePost',
+    timestamps: false,
+  }
+);
